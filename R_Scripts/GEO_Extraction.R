b9f07276c8b17521b835d7d56cc82eb564ffa531
Jillian Woodley <j.s.woodley22@gmail.com>
2025-10-19 17:51:53 -0500

    Update GEO_Extraction.R

--- R_Scripts/GEO_Extraction.R
+++ R_Scripts/GEO_Extraction.R
@@ -258,9 +258,9 @@ get_single_gse_full = function(geo_id) {
 #---------------------------------
 # Main workflow
 #---------------------------------
-gse_df = read.csv("~/Desktop/ids.csv", header = FALSE, stringsAsFactors = FALSE)
+gse_df = read.csv("gse.csv", header = FALSE, stringsAsFactors = FALSE)
 geo_ids = convert_to_gse(as.character(gse_df[[1]]))
-geo_ids = geo_ids[1:30]  # adjust for testing comment out this line to run the entire thing
+geo_ids = geo_ids[1:10]  # adjust for testing comment out this line to run the entire thing
 
 plan(multisession, workers = 4)
 handlers(global = TRUE)
